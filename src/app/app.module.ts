030d93386261faf342b244003f14033210154fbd
phanvanloi <106456389+phanvanloi@users.noreply.github.com>
2023-05-24 19:54:55 +0700

    create

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
